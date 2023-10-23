bee589914c851c613318ad2764d26b9acec8fe7c
KobayashiYoh <y.kobayashi@4nonome.com>
2023-10-23 23:21:08 +0900

    クリックでオブジェクト自身を削除するdestroyメソッドを追加

--- ClickGame/Assets/FlyController.cs
+++ ClickGame/Assets/FlyController.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class FlyController : MonoBehaviour
 {
+    void destroy()
+    {
+        GameObject clickedGameObject = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hitSprite = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+        if (hitSprite == true)
+        {
+            clickedGameObject = hitSprite.transform.gameObject;
+            if (clickedGameObject.tag == "Fly")
+            {
+                Destroy(clickedGameObject);
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            Destroy(gameObject);
+        if (Input.GetMouseButtonDown(0))
+        {
+            destroy();
         }
+
+        // float rndX = Random.Range(-0.1f, 0.1f);
+        // float rndY = Random.Range(-0.1f, 0.1f);
+        // transform.Translate(rndX, rndY, 0);
     }
 }
